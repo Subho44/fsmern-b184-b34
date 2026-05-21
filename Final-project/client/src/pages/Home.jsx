eafe2edc98f4861d628cf778d266dc7a5cde0e1d
Subho44 <98902921+Subho44@users.noreply.github.com>
2026-05-21 21:06:07 +0530

    ok
    ok

--- Final-project/client/src/pages/Home.jsx
+++ Final-project/client/src/pages/Home.jsx
@@ -20,7 +20,7 @@ const Home = () => {
         },[]);
 
   return <>
-  <div>
+  <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
     {
         courses.map(x=>(
             <Coursecard
